742b9c5c3435c4ed6a9fa95f156643e460710641
Jakob Lingel <jlingel68@gmail.com>
2025-06-01 19:20:20 +0200

    Add endpoints

---
+++ src/api/arbeitsweise/routes/arbeitsweise.ts
@@ -0,0 +1,7 @@
+/**
+ * arbeitsweise router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::arbeitsweise.arbeitsweise');
---
+++ src/api/simple-list/routes/simple-list.ts
@@ -0,0 +1,7 @@
+/**
+ * simple-list router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::simple-list.simple-list');
